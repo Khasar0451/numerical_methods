81e2dc180e8072da20a3eb867b6fb91a229e248f
Executed419 <lucker10@interia.pl>
2023-03-28 21:55:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import pandas as pan
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+
+def sell
+
+
+def buy
+
+
+def decide
+
 
 
 def alfa(n):
@@ -12,21 +23,16 @@ def ema(n, list):
     for i in range(n, len(list)):  # dla każdego elementu listy (od n) wylicz średnią kroczącą (do n danych wstecz)
         x = 0
         y = 0
-        print("i ", i)
-        for k in range(0, n):  # n elementow
+        for k in range(0, n):
             x += list[i - k] * pow(1 - a, k)
-        #       print("x ", x)
-        #       print("val ", list[i - k])
         for k in range(0, n):
             y += pow(1 - a, k)
-        #      print("y ", y)
 
         emaList.append(x / y)
-    #    print(emaList)
     return emaList
 
 
-data = pan.read_csv("int.csv")
+data = pan.read_csv("11B.WA.csv")
 ema12 = ema(12, data.High)
 ema26 = ema(26, data.High)
 macd = []
@@ -37,12 +43,16 @@ for j in range(0, len(ema26)):
 
 signal = ema(9, macd)
 
-dates = data.Date[26:]
-datesS = data.Date[26 + 9:]
-plt.plot(dates, macd, color='r', label='MACD')
-plt.plot(datesS, signal, color='g', label='SIGNAL')
-# plt.plot(data.High, color='b', label='INPUT')
+print(data.Date)
+
+figure, axes = plt.subplots()
+axes.plot(data.Date[26:], macd, color='r', label='MACD')
+axes.plot(data.Date[26 + 9:], signal, color='g', label='SIGNAL')
+axes.plot(data.Date[26:], data.High[26:], color='b', label='INPUT')
+
+locator = mdates.AutoDateLocator()
+axes.xaxis.set_major_locator(locator)
+axes.tick_params(axis="x", rotation=-15)
+
 plt.legend()
 plt.show()
-print(macd)
-# print(signal)
